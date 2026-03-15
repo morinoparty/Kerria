f0f9e45bf65298d48b85314fceea4d8c78928b0d
nlkomaru <nikomaru@nikomaru.dev>
2026-03-15 18:49:34 +0900

    🔄 Migrate docs from Docusaurus to Fumadocs (Next.js)

--- docs/src/components/uuid-example.tsx
+++
@@ -1,9 +0,0 @@
-import { v4 as uuidv4, v7 as uuidv7 } from "uuid";
-
-export const UUIDV7 = () => {
-    return <>{uuidv7()}</>;
-};
-
-export const UUIDV4 = () => {
-    return <>{uuidv4()}</>;
-};
